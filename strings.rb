d165883688d1495e9cd276c7826533f03658f17f
candaceww <candacew224@gmail.com>
2018-07-24 16:31:19 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -13,3 +13,5 @@ puts "Dear #{guest_name}"
 puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than a day before the party."
 
 puts "Sincerely,"
+
+puts "#{host_name}"

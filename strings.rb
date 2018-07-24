e724348cb70f546a8d71635bd17110c20999ac32
candaceww <candacew224@gmail.com>
2018-07-24 16:30:54 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -1,4 +1,15 @@
-puts "Welcome, please type in your name."
+puts "Welcome, please type in your guests."
 guest_name=gets.strip
 puts "Thank you, which party are you going to?"
 party_name=gets.strip
+puts "Ok when is this party?"
+date=gets.strip
+puts "What time will the party be?"
+time=gets.strip
+puts "Who's hosting?"
+host_name=gets.strip
+puts "Dear #{guest_name}"
+
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than a day before the party."
+
+puts "Sincerely,"
